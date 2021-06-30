a32518fb509e7e26f0c5ac534e4db18f98828974
Faizan Mehmoos <39873595+faizan3165@users.noreply.github.com>
2021-06-30 14:57:57 +0500

    cors config

--- index.js
+++ index.js
@@ -15,6 +15,17 @@ app.use(express.json({ limit: '30mb', extended: true }));
 app.use(express.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
+var whitelist = ['http://localhost:5000', 'https://memories-react-prject.herokuapp.com/posts']
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (whitelist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
+
 app.use('/posts', postRoutes);
 
 app.get('/', (req, res) => {
@@ -24,7 +35,7 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 mongoose
-	.connect(process.env.CONNECTION_URL, {
+	.connect(process.env.LOCAL_DB, {
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
 		useCreateIndex: true
