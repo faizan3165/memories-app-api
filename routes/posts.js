bda0e8005c96b923f76cb0b3eb533bf6712c4870
Faizan Mehmoos <39873595+faizan3165@users.noreply.github.com>
2021-06-30 15:55:28 +0500

    cors config

--- routes/posts.js
+++ routes/posts.js
@@ -12,7 +12,7 @@ const whitelist = [
 
 const corsOptionsDelegate = function(req, callback){
 	const corsOptions = null;
-	if (whitelist.indexOf(req.header('Origin')) !== -1 || !origin) {
+	if (whitelist.indexOf(req.header('Origin')) !== -1) {
 		corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
 	} else {
 		corsOptions = { origin: false }; // disable CORS for this request
