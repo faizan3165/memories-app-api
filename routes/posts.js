456985cb1827cb3852a970cd682306c1d7b7b82d
Faizan Mehmoos <39873595+faizan3165@users.noreply.github.com>
2021-06-30 16:40:30 +0500

    cors config 2

--- routes/posts.js
+++ routes/posts.js
@@ -5,22 +5,6 @@ import { getPosts, getPost, createPost, updatePost, deletePost, likePost } from 
 
 const router = express.Router();
 
-// var whitelist = [
-// 	'http://localhost:5000',
-// 	'https://serene-river-10220.herokuapp.com/posts'
-// ];
-
-// var corsOptionsDelegate = function(req, callback){
-// 	var corsOptions;
-// 	if (whitelist.indexOf(req.header('Origin')) !== -1) {
-// 		corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
-// 	} else {
-// 		corsOptions = { origin: false }; // disable CORS for this request
-// 	}
-// 	callback(null, corsOptions); // callback expects two parameters: error and options
-// };
-
-// cors(corsOptionsDelegate),
 router.get('/',  getPosts);
 
 router.post('/', createPost);
