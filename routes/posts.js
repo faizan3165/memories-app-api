454207a94817f728a8508a32780860b2c6c38768
Faizan Mehmoos <39873595+faizan3165@users.noreply.github.com>
2021-06-30 16:12:40 +0500

    cors config

--- routes/posts.js
+++ routes/posts.js
@@ -1,26 +1,27 @@
 import express from 'express';
-import cors from 'cors';
+// import cors from 'cors';
 
 import { getPosts, getPost, createPost, updatePost, deletePost, likePost } from '../controllers/posts.js';
 
 const router = express.Router();
 
-var whitelist = [
-	'http://localhost:5000',
-	'https://serene-river-10220.herokuapp.com/posts'
-];
+// var whitelist = [
+// 	'http://localhost:5000',
+// 	'https://serene-river-10220.herokuapp.com/posts'
+// ];
 
-var corsOptionsDelegate = function(req, callback){
-	var corsOptions;
-	if (whitelist.indexOf(req.header('Origin')) !== -1) {
-		corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
-	} else {
-		corsOptions = { origin: false }; // disable CORS for this request
-	}
-	callback(null, corsOptions); // callback expects two parameters: error and options
-};
+// var corsOptionsDelegate = function(req, callback){
+// 	var corsOptions;
+// 	if (whitelist.indexOf(req.header('Origin')) !== -1) {
+// 		corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+// 	} else {
+// 		corsOptions = { origin: false }; // disable CORS for this request
+// 	}
+// 	callback(null, corsOptions); // callback expects two parameters: error and options
+// };
 
-router.get('/', cors(corsOptionsDelegate), getPosts);
+// cors(corsOptionsDelegate),
+router.get('/',  getPosts);
 
 router.post('/', createPost);
 
